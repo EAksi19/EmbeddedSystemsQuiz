6de4e0170415b6032193689940a2ae79cef761cd
EAksi19 <107687591+EAksi19@users.noreply.github.com>
2022-10-07 23:49:45 +0100

    Added the files

---
+++ main.py
@@ -0,0 +1,98 @@
+# https://realpython.com/python-quiz-application/
+
+print("WELCOME TO THE GAME SHOW, TO EMBEDDED OR NOT EMBEDDED SYSTEM!")
+name = input("\nWhat is your name? ")
+
+print("\nLet us start {0}".format(name))
+
+print("Only anwser with 'Yes' or 'No' ")
+
+#Eren
+print("\nQUESTION 1!")
+Q1 = input("\nIs bread an embedded system? ")
+if Q1 == "No":
+  print("Well done, you got the easiest correct")
+elif Q1 == "no":
+  print("You got it correct but your english teacher wants you to capitalize the beginning of your sentences")
+else:
+  print("You got it wrong! But don't worry you can always live with the regret of being born")
+
+#Ben
+print("\nQUESTION 2!")
+Q2 = input("\nIs a phone an embedded system? ")
+if Q2 == "Yes":
+ print("You got this question correct, congrats!")
+elif Q2 == "yes":
+ print("You got this question correct, congrats!")
+elif Q2 == "e":
+ print("e \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne \ne")
+else:
+  print("You got this question wrong, you should study more.")
+
+
+
+#Ellis
+print("\nQUESTION 3!")
+Q3 = input("\nAre backup sensors embedded systems? ")
+if Q3 == "yes":
+  print ("Congrats your getting smarter")
+elif Q3 == "Yes":
+  print ("Congrats your getting smarter")
+else:
+  print("WRONG")
+
+
+#Eren
+print("\nQUESTION 4!")
+Q4 = input("\nAre credit cards embedded systems? ")
+if Q4 == "Yes":
+  print("Oooh, you got it correct!")
+elif Q4 == "yes":
+  print("Ooooooooh, that was correct!!!")
+else:
+  print("You got it wrong but don't worry, it was a hard one")
+
+  
+#Ben
+print("\nQUESTION 5!")
+Q5 = input("\nIs an electric car an embedded system? ")
+if Q5 == "Yes":
+  print("Correct")
+elif Q5 == "yes":
+  print("Correct")
+else:
+  print("Incorrect")
+
+
+#Ellis
+print("\nQUESTION 6!")
+Q6 = input("\nIS A MICROWAVE AN EMBEDDED SYSTEM? ")
+if Q6 == "Yes":
+  print("IT IS CORRECT!")
+elif Q6 == "yes":
+  print("CORRECT!!!")
+else:
+  print("you got it wrong :(")
+
+
+#Eren
+print("\nQUESTION 7")
+Q7 = input("\nIs a digital camera a embedded system? ")
+if Q7 == "Yes":
+  print("WOOOOOOO CORRECT!!!!!!")
+elif Q7 == "yes":
+  print("CORRECT!!!!!!!!!!!!!!")
+elif Q7 == "snap":
+  print("HEY I WASN'T POSING!")
+else:
+  print("Oooh not so correct...")
+
+#Ben
+print("\nQUESTION 8")
+Q8 = input("\nIs a computer an embedded system? ")
+if Q8 == "No":
+  print("That is very correct")
+elif Q8 == "no":
+  print("that is very correct")
+else:
+  print("wRoNg!!!")
